67e5fc3eea30f159ed284badbabe47f4a4c1b8a7
g-otn <g-otn@hotmail.com>
2020-03-08 18:09:44 -0300

    update database credentials

--- src/br/com/solstice/notecommerce/dao/connections/MySQLConnection.java
+++ src/br/com/solstice/notecommerce/dao/connections/MySQLConnection.java
@@ -15,8 +15,8 @@ public class MySQLConnection {
 	public static Connection getConecction() throws ClassNotFoundException, SQLException {
 		driver = "com.mysql.jdbc.Driver";
 		url = "jdbc:mysql://localhost:3306/notecommerce_db";
-		user = "root";
-		password = "Th@67245261";
+		user = "notecommerce_user";
+		password = "notecommerce_password";
 
 		try {
 			Class.forName(driver);
