c048291aa5ed52c723b2fffae0bef2efe426db83
g-otn <g-otn@hotmail.com>
2020-03-09 10:58:58 -0300

    add generateUrl static method

--- src/br/com/solstice/notecommerce/domain/product/file/ProductFile.java
+++ src/br/com/solstice/notecommerce/domain/product/file/ProductFile.java
@@ -1,12 +1,27 @@
 package br.com.solstice.notecommerce.domain.product.file;
 
 import java.io.InputStream;
+import java.util.UUID;
 
 public class ProductFile {
 	
 	private InputStream fileContent; // For upload and save file only
 	private String url;              // For use in JSP
 
+	public static String generateUrl(String fileName) {
+		String url = "/note-commerce/static/images/";
+		String[] fileNameParts = fileName.split("\\.");
+		String uuid = UUID.randomUUID().toString().replace("-", "");
+		
+		if (fileNameParts.length > 1) {
+			url += uuid + "." + fileNameParts[1];
+		} else {
+			url += uuid + ".unknown";
+		}
+		System.out.println("file url: " + url);
+		return url;
+	}
+	
 	public InputStream getFileContent() {
 		return fileContent;
 	}
