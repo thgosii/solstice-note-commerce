1b299993dc464ee8de1943fb94ccff651bab94e6
g-otn <g-otn@hotmail.com>
2020-03-09 07:07:16 -0300

    create populate-tables.sql with admin account and brand data

---
+++ sql/populate-tables.sql
@@ -0,0 +1,14 @@
+-- Admin account
+INSERT INTO `notecommerce_db`.`users` (`usr_id`, `usr_email`, `usr_password`, `usr_role`) VALUES ('1', 'admin@admin.com', 'admin', 'admin');
+
+
+-- Brands
+INSERT INTO `notecommerce_db`.`brands` (`brd_id`, `brd_name`) VALUES ('1', 'Acer');
+INSERT INTO `notecommerce_db`.`brands` (`brd_id`, `brd_name`) VALUES ('2', 'Asus');
+INSERT INTO `notecommerce_db`.`brands` (`brd_id`, `brd_name`) VALUES ('3', 'Dell');
+INSERT INTO `notecommerce_db`.`brands` (`brd_id`, `brd_name`) VALUES ('4', 'Hp');
+INSERT INTO `notecommerce_db`.`brands` (`brd_id`, `brd_name`) VALUES ('5', 'Lenovo');
+INSERT INTO `notecommerce_db`.`brands` (`brd_id`, `brd_name`) VALUES ('6', 'Positivo');
+INSERT INTO `notecommerce_db`.`brands` (`brd_id`, `brd_name`) VALUES ('7', 'Samsung');
+
+-- Products
