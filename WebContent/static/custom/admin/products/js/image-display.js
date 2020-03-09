3a06b7e101c7087121c539e79364199a5785342b
g-otn <g-otn@hotmail.com>
2020-03-08 23:55:58 -0300

    create: image-display.js to preview input file images

---
+++ WebContent/static/custom/admin/products/js/image-display.js
@@ -0,0 +1,18 @@
+$(document).ready(() => {
+
+  // https://stackoverflow.com/a/57272491/11138267
+  const toBase64 = file => new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = error => reject(error)
+  })
+
+  $('#image').on('change', async () => {
+    let imgElement = $('<img>')
+    let imageBase64 = await toBase64($('#image').prop('files')[0])
+    imgElement.prop('src', imageBase64)
+    $('.product-img-display').html(imgElement)
+  })
+
+})
