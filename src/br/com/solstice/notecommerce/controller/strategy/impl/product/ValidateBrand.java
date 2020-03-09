d0b8f9920d048af01023e24f44393a594d6ca109
g-otn <g-otn@hotmail.com>
2020-03-09 07:16:03 -0300

    add existent brand verification

--- src/br/com/solstice/notecommerce/controller/strategy/impl/product/ValidateBrand.java
+++ src/br/com/solstice/notecommerce/controller/strategy/impl/product/ValidateBrand.java
@@ -1,6 +1,7 @@
 package br.com.solstice.notecommerce.controller.strategy.impl.product;
 
 import br.com.solstice.notecommerce.controller.strategy.IStrategy;
+import br.com.solstice.notecommerce.dao.impl.BrandDAO;
 import br.com.solstice.notecommerce.domain.DomainEntity;
 import br.com.solstice.notecommerce.domain.product.Product;
 import br.com.solstice.notecommerce.domain.product.brand.Brand;
@@ -10,12 +11,15 @@ public class ValidateBrand implements IStrategy {
 	@Override
 	public String process(DomainEntity entity) {
 		Brand brand = ((Product) entity).getBrand();
+		BrandDAO brandDAO = new BrandDAO();
 
 		if (null == brand) {
 			return "brand: A marca é obrigatória";
-		} else if (/*dao*/)  {
+		} else if (0 == brandDAO.consult(brand, "findById").size())  {
 			return "brand: A marca é inválida\n";
 		}
+		
+		return null;
 	}
 
 }
