5370e83d666602fe7063ab78db18e601001db841
g-otn <g-otn@hotmail.com>
2020-03-10 06:29:23 -0300

    fix: validation

--- src/br/com/solstice/notecommerce/controller/strategy/impl/product/ValidateStorage.java
+++ src/br/com/solstice/notecommerce/controller/strategy/impl/product/ValidateStorage.java
@@ -13,12 +13,12 @@ public class ValidateStorage implements IStrategy {
 		StringBuilder sb = new StringBuilder();
 		
 		if (hd == -1 && ssd == -1) {
-			sb.append("hd: É necessário possuir pelo menos HD ou SSD\n");
+			sb.append("hd-capacity: É necessário possuir pelo menos HD ou SSD\n");
 		} else {
-			if (hd < 0 || hd > 5000) {
+			if (ssd == -1 && (hd < 0 || hd > 5000)) {
 				sb.append("hd-capacity: Quantidade inválida\n");
 			}
-			if (ssd < 0 || ssd > 5000) {
+			if (hd == -1 && (ssd < 0 || ssd > 5000)) {
 				sb.append("ssd-capacity: Quantidade inválida\n");
 			}
 		}
