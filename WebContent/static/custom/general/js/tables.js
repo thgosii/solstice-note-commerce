56cc39892d42045684344905ad58d9a524e3623e
g-otn <g-otn@hotmail.com>
2020-03-10 10:02:35 -0300

    fix: jquery selector

--- WebContent/static/custom/general/js/tables.js
+++ WebContent/static/custom/general/js/tables.js
@@ -6,7 +6,7 @@ $(document).ready(() => {
 
       // Gets the id from the table row
       const id = a.data('id')
-      $('#delete-modal-btn').val(id)
+      $('#id').val(id)
 
       // Shows the text in the modal
       const text = a.parent().next('.title').text()
