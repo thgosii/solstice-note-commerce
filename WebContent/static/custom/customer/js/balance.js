c3bea15f7aa377e95d01f74b9e7b0fa8280deb9a
g-otn <g-otn@hotmail.com>
2020-04-14 03:17:37 -0300

    fix: 0 balance showing as 'unknown'

--- WebContent/static/custom/customer/js/balance.js
+++ WebContent/static/custom/customer/js/balance.js
@@ -2,7 +2,7 @@ $(document).ready(() => {
 
   $.ajax('/note-commerce/customer/balance?operation=consult')
     .then(data => {
-      const currentBalanceText = (Number(data.balance) || 'desconhecido')
+      const currentBalanceText = Number(data.balance)
         .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
       $('#navbar-customer-balance').text(currentBalanceText)
     })
