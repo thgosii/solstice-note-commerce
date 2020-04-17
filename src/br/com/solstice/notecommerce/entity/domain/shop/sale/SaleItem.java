78c3332806ffc7faaa44a6d3b9e39214e8ec8b34
g-otn <g-otn@hotmail.com>
2020-04-17 06:24:09 -0300

    refactor: use lombok annotations on SaleItem

--- src/br/com/solstice/notecommerce/entity/domain/shop/sale/SaleItem.java
+++ src/br/com/solstice/notecommerce/entity/domain/shop/sale/SaleItem.java
@@ -2,40 +2,21 @@ package br.com.solstice.notecommerce.entity.domain.shop.sale;
 
 import br.com.solstice.notecommerce.entity.domain.DomainEntity;
 import br.com.solstice.notecommerce.entity.domain.product.Product;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class SaleItem extends DomainEntity {
 
 	private Product product;
 	private int quantity;
 	private double subTotal;
-
-	public Product getProduct() {
-		return product;
-	}
-
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	public double getSubTotal() {
-		return subTotal;
-	}
-
-	public void setSubTotal(double subTotal) {
-		this.subTotal = subTotal;
-	}
-
-	@Override
-	public String toString() {
-		return "SaleItem [quantity=" + quantity + ", subTotal=" + subTotal + ", product=" + product + "]";
-	}
 
 }
