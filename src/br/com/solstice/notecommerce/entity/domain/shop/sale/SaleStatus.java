e5019c19df02e5dadfb5dacda3c87c606e1ca9b7
g-otn <g-otn@hotmail.com>
2020-04-17 05:36:31 -0300

    update comments

--- src/br/com/solstice/notecommerce/entity/domain/shop/sale/SaleStatus.java
+++ src/br/com/solstice/notecommerce/entity/domain/shop/sale/SaleStatus.java
@@ -1,9 +1,9 @@
 package br.com.solstice.notecommerce.entity.domain.shop.sale;
 
 public enum SaleStatus {
-	
-	DELIVERED,
-	ON_DELIVERY,
-	PAYMENT_APPROVED
+
+	PAYMENT_APPROVED,	// Customer just confirmed sale, payment validated and approved
+	ON_DELIVERY,		// Admin confirmed start of delivery of sale products
+	DELIVERED			// Admin confirmed that products were delivered, no more actions
 
 }
--- src/br/com/solstice/notecommerce/entity/domain/trade/TradeStatus.java
+++ src/br/com/solstice/notecommerce/entity/domain/trade/TradeStatus.java
@@ -4,6 +4,6 @@ public enum TradeStatus {
 	
 	AWAITING_AUTHORIZATION, // Trade requested by customer, waiting authorization
 	AUTHORIZED,			    // Trade authorized by admin, now waiting for product to arrive
-	PRODUCT_RECEIVED		// Items were received by the store from the client, 
+	PRODUCT_RECEIVED		// Items were received by the store from the client, balance updated, no more actions
 	
 }
