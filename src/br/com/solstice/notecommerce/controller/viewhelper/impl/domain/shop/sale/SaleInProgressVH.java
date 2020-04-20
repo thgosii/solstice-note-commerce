7c741a7ddcfbe924796972f0d3b2e813a46b30ba
Thiago Bezerra <thiago.bezsilva@gmail.com>
2020-04-20 17:21:23 -0300

    changed json implementation to gson

--- src/br/com/solstice/notecommerce/controller/viewhelper/impl/domain/shop/sale/SaleInProgressVH.java
+++ src/br/com/solstice/notecommerce/controller/viewhelper/impl/domain/shop/sale/SaleInProgressVH.java
@@ -3,22 +3,17 @@ package br.com.solstice.notecommerce.controller.viewhelper.impl.domain.shop.sale
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.google.gson.Gson;
 
 import br.com.solstice.notecommerce.controller.viewhelper.IViewHelper;
 import br.com.solstice.notecommerce.entity.Entity;
 import br.com.solstice.notecommerce.entity.Result;
 import br.com.solstice.notecommerce.entity.domain.shop.sale.SaleInProgress;
-import br.com.solstice.notecommerce.entity.domain.shop.sale.SaleItem;
 import br.com.solstice.notecommerce.entity.domain.user.customer.address.Address;
 import br.com.solstice.notecommerce.entity.domain.user.customer.credit_card.CreditCard;
 
@@ -36,45 +31,45 @@ public class SaleInProgressVH implements IViewHelper {
 
 		if (operation.equals("save")) {
 			int step = Integer.valueOf(request.getParameter("step"));
-			
+
 			switch (step) {
-				case 1: {
-					// Save Address
-					Long addressId = Long.valueOf(request.getParameter("address"));
-	
-					SaleInProgress saleInProgress = new SaleInProgress();
-					saleInProgress.setBalanceUsage(-1);
-					
-					Address address = new Address();
-					address.setId(addressId);
-	
-					saleInProgress.setAddress(address);
-	
-					return saleInProgress;
+			case 1: {
+				// Save Address
+				Long addressId = Long.valueOf(request.getParameter("address"));
+
+				SaleInProgress saleInProgress = new SaleInProgress();
+				saleInProgress.setBalanceUsage(-1);
+
+				Address address = new Address();
+				address.setId(addressId);
+
+				saleInProgress.setAddress(address);
+
+				return saleInProgress;
+			}
+			case 2: {
+				// Save Credit Card
+				double balance = 0.0;
+				Long creditCardId = null;
+
+				if (request.getParameter("balance") != null) {
+					balance = Double.valueOf(request.getParameter("balance"));
 				}
-				case 2: {
-					// Save Credit Card
-					double balance = 0.0;
-					Long creditCardId = null;
-	
-					if (request.getParameter("balance") != null) {
-						balance = Double.valueOf(request.getParameter("balance"));
-					}
-	
-					if (request.getParameter("creditCard") != null) {
-						creditCardId = Long.valueOf(request.getParameter("creditCard"));
-					}
-	
-					SaleInProgress saleInProgress = new SaleInProgress();
-					saleInProgress.setBalanceUsage(balance);
-	
-					CreditCard creditCard = new CreditCard();
-					creditCard.setId(creditCardId);
-	
-					saleInProgress.setCreditCard(creditCard);
-	
-					return saleInProgress;
+
+				if (request.getParameter("creditCard") != null) {
+					creditCardId = Long.valueOf(request.getParameter("creditCard"));
 				}
+
+				SaleInProgress saleInProgress = new SaleInProgress();
+				saleInProgress.setBalanceUsage(balance);
+
+				CreditCard creditCard = new CreditCard();
+				creditCard.setId(creditCardId);
+
+				saleInProgress.setCreditCard(creditCard);
+
+				return saleInProgress;
+			}
 			}
 		} else if (operation.equals("consult")) {
 			return new SaleInProgress();
@@ -101,18 +96,12 @@ public class SaleInProgressVH implements IViewHelper {
 
 			response.setContentType("application/json");
 			response.setCharacterEncoding("UTF-8");
-			
-			try {
-				JSONObject jsonObject = new JSONObject();
-				
-				jsonObject.put("saleInProgress", saleInProgress);
-				
-				PrintWriter writer = response.getWriter();
-				writer.write(jsonObject.toString());
-				writer.flush();
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
+
+			String json = new Gson().toJson(saleInProgress);
+
+			PrintWriter writer = response.getWriter();
+			writer.print(json);
+			writer.flush();
 		}
 	}
 
