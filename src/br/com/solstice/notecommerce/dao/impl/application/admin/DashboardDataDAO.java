567fd837fa49630fd9ae1d6527afff69874c737f
g-otn <g-otn@hotmail.com>
2020-04-21 07:32:51 -0300

    feat: consult method except sql strings

--- src/br/com/solstice/notecommerce/dao/impl/application/admin/DashboardDataDAO.java
+++ src/br/com/solstice/notecommerce/dao/impl/application/admin/DashboardDataDAO.java
@@ -1,13 +1,25 @@
 package br.com.solstice.notecommerce.dao.impl.application.admin;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import br.com.solstice.notecommerce.dao.AbstractDAO;
 import br.com.solstice.notecommerce.entity.Entity;
+import br.com.solstice.notecommerce.entity.application.admin.dashboard.BrandSale;
+import br.com.solstice.notecommerce.entity.application.admin.dashboard.DashboardData;
+import br.com.solstice.notecommerce.entity.application.admin.dashboard.MostSoldProduct;
+import br.com.solstice.notecommerce.entity.application.admin.dashboard.MostTradedProduct;
+import br.com.solstice.notecommerce.entity.application.admin.dashboard.ProductTableRow;
+import br.com.solstice.notecommerce.entity.application.admin.dashboard.RegionSale;
+import br.com.solstice.notecommerce.entity.domain.product.Product;
 
 public class DashboardDataDAO extends AbstractDAO {
 
@@ -34,20 +46,109 @@ public class DashboardDataDAO extends AbstractDAO {
 
 	@Override
 	public List<Entity> consult(Entity entity, String operation) {
-		
 		openConnection();
+		
+		DashboardData dd = (DashboardData) entity;
 		
 		PreparedStatement pstm = null;
 		ResultSet rs = null;
 		
 		String sql = "";
 		
+		List<Entity> dashboardDataList = new ArrayList<Entity>();
+		
 		try {
 
+			// brandSaleGraph
+			sql = "";
 			pstm = connection.prepareStatement(sql);
+			pstm.setDate(1, Date.valueOf(dd.getMinDate()));
+			pstm.setDate(2, Date.valueOf(dd.getMaxDate()));
+			rs = pstm.executeQuery();
 			
-			// TODO
+			Map<String, List<BrandSale>> brandSaleGraph = new HashMap<String, List<BrandSale>>();
+			while (rs.next()) {
+				String brandName = rs.getString("brand");
+				if (!brandSaleGraph.containsKey(brandName)) {
+					brandSaleGraph.put(brandName, new ArrayList<BrandSale>());
+				}
+				
+				LocalDate date = LocalDate.parse(rs.getString("date"));
+				Long amount = rs.getLong("amount");
+				brandSaleGraph.get(brandName).add(new BrandSale(date, amount));
+			}
 			
+			
+			pstm.close();
+
+			
+			// regionSaleMap
+			sql = "";
+			pstm = connection.prepareStatement(sql);
+			pstm.setDate(1, Date.valueOf(dd.getMinDate()));
+			pstm.setDate(2, Date.valueOf(dd.getMaxDate()));
+			rs = pstm.executeQuery();
+			
+			Map<String, Long> regionSaleMap = new HashMap<String, Long>();
+			while (rs.next()) {
+				String stateInitials = rs.getString("state").toLowerCase();
+				Long amount = rs.getLong("amount");
+				regionSaleMap.put(stateInitials, amount);
+			}
+			
+			
+			pstm.close();
+
+			
+			
+			// mostSoldProductsTable
+			sql = "";
+			pstm = connection.prepareStatement(sql);
+			pstm.setDate(1, Date.valueOf(dd.getMinDate()));
+			pstm.setDate(2, Date.valueOf(dd.getMaxDate()));
+			rs = pstm.executeQuery();
+			
+		    List<ProductTableRow> mostSoldProductsTable = new ArrayList<ProductTableRow>();
+			while (rs.next()) {
+				Product product = new Product();
+				product.setId(rs.getLong("id"));
+				product.setTitle(rs.getString("title"));
+				Long amount = rs.getLong("amount");
+				
+				mostSoldProductsTable.add(new ProductTableRow(amount, product));
+			}
+			
+			
+			pstm.close();
+
+			
+			
+			// mostTradedProductsTable
+			sql = "";
+			pstm = connection.prepareStatement(sql);
+			pstm.setDate(1, Date.valueOf(dd.getMinDate()));
+			pstm.setDate(2, Date.valueOf(dd.getMaxDate()));
+			rs = pstm.executeQuery();
+
+		    List<ProductTableRow> mostTradedProductsTable = new ArrayList<ProductTableRow>();
+			while (rs.next()) {
+				Product product = new Product();
+				product.setId(rs.getLong("id"));
+				product.setTitle(rs.getString("title"));
+				Long amount = rs.getLong("amount");
+				
+				mostTradedProductsTable.add(new ProductTableRow(amount, product));
+			}
+			
+			
+			pstm.close();
+			
+			
+			
+			
+			// DashboardData
+			dashboardDataList.add(new DashboardData(brandSaleGraph, regionSaleMap, mostSoldProductsTable, mostTradedProductsTable));
+			return dashboardDataList;
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -59,9 +160,6 @@ public class DashboardDataDAO extends AbstractDAO {
 			}
 			closeConnection();
 		}
-
-		
-		
 		
 		return null;
 	}
