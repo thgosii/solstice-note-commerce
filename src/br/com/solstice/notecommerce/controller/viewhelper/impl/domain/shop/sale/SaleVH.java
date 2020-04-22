d7a7b480046328c4e9ea529791877d058cee740b
Thiago Bezerra <thiago.bezsilva@gmail.com>
2020-04-22 15:07:36 -0300

    created sale dao and sale vh

---
+++ src/br/com/solstice/notecommerce/controller/viewhelper/impl/domain/shop/sale/SaleVH.java
@@ -0,0 +1,26 @@
+package br.com.solstice.notecommerce.controller.viewhelper.impl.domain.shop.sale;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.solstice.notecommerce.controller.viewhelper.IViewHelper;
+import br.com.solstice.notecommerce.entity.Entity;
+import br.com.solstice.notecommerce.entity.Result;
+
+public class SaleVH implements IViewHelper {
+
+	@Override
+	public Entity getEntity(HttpServletRequest request) {
+		return null;
+	}
+
+	@Override
+	public void setView(Result result, HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+		
+	}
+
+}
