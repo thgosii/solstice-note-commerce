8195735d19d52d23c61219d3024b0d94598198c6
g-otn <g-otn@hotmail.com>
2020-05-03 03:02:33 -0300

    refactor: status to support devolution and exchanges

--- src/br/com/solstice/notecommerce/entity/domain/trade/TradeStatus.java
+++ src/br/com/solstice/notecommerce/entity/domain/trade/TradeStatus.java
@@ -3,6 +3,7 @@ package br.com.solstice.notecommerce.entity.domain.trade;
 public enum TradeStatus {
 	
 	AWAITING_AUTHORIZATION, // Trade requested by customer, waiting authorization
+	DENIED,					// Trade denied by admin, no more actions
 	AUTHORIZED,			    // Trade authorized by admin, now waiting for product to arrive
 	PRODUCT_RECEIVED,		// Trade items were received by the store from the client. (Devolution only: balance updated, no more actions)
 	REPLACEMENT_ON_DELIVERY,// (Exchange only): Replacement products are being delivered
