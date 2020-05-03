686fb72a7de412c98884993d3cddd908b16c6a18
g-otn <g-otn@hotmail.com>
2020-05-03 05:33:22 -0300

    update comment

--- src/br/com/solstice/notecommerce/entity/domain/trade/TradeStatus.java
+++ src/br/com/solstice/notecommerce/entity/domain/trade/TradeStatus.java
@@ -7,6 +7,6 @@ public enum TradeStatus {
 	AUTHORIZED,			    // Trade authorized by admin, now waiting for product to arrive
 	PRODUCT_RECEIVED,		// Trade items were received by the store from the client. (Devolution only: balance updated, no more actions)
 	REPLACEMENT_ON_DELIVERY,// (Exchange only): Replacement products are being delivered
-	REPLACEMENT_DELIVERED   // (Exchange only): Replacement products were delivered
+	REPLACEMENT_DELIVERED   // (Exchange only): Replacement products were delivered (or balanced updated if there is no stock)
 	
 }
