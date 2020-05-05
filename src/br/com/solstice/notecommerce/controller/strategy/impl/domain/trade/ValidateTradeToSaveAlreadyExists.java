514f5a96f5c6dab09f2bb50612b40b47499f6337
g-otn <g-otn@hotmail.com>
2020-05-05 01:59:22 -0300

    feat: prevent starting trades based on already finished devolutions and ongoing exchanges

--- src/br/com/solstice/notecommerce/controller/strategy/impl/domain/trade/ValidateTradeToSaveAlreadyExists.java
+++ src/br/com/solstice/notecommerce/controller/strategy/impl/domain/trade/ValidateTradeToSaveAlreadyExists.java
@@ -3,10 +3,14 @@ package br.com.solstice.notecommerce.controller.strategy.impl.domain.trade;
 import java.util.Arrays;
 import java.util.List;
 
+import com.sun.org.apache.bcel.internal.generic.I2F;
+
 import br.com.solstice.notecommerce.controller.strategy.AbstractStrategy;
 import br.com.solstice.notecommerce.dao.impl.domain.trade.TradeDAO;
 import br.com.solstice.notecommerce.entity.Entity;
 import br.com.solstice.notecommerce.entity.domain.trade.Trade;
+import br.com.solstice.notecommerce.entity.domain.trade.TradeStatus;
+import br.com.solstice.notecommerce.entity.domain.trade.TradeType;
 
 public class ValidateTradeToSaveAlreadyExists extends AbstractStrategy {
 	
@@ -21,13 +25,31 @@ public class ValidateTradeToSaveAlreadyExists extends AbstractStrategy {
 	@Override
 	public String process(Entity entity) {
 		Trade trade = (Trade) entity;
+		TradeType originalTradeType = trade.getType();
+		System.out.println("\noriginal: " + originalTradeType);
 		TradeDAO tradeDAO = new TradeDAO();
 		
+		// Find if there is any type of trade with this sale item already in progress
+		
+		trade.setType(TradeType.DEVOLUTION);
+		System.out.println("type: " + ((Trade) entity).getType());
 		List<Entity> listEntities = tradeDAO.consult(trade, "findFromSaleAndProduct");
 		if (listEntities.size() != 0) {
-			return "Esse produto já faz está em troca ou devolução";
+			if (((Trade) listEntities.get(0)).getStatus() != TradeStatus.DENIED) {
+				return "Esse produto já faz parte de uma devolução em andamento ou que já atualizou o seu saldo";
+			}
 		}
 		
+		trade.setType(TradeType.EXCHANGE);
+		listEntities = tradeDAO.consult(trade, "findFromSaleAndProduct");
+		if (listEntities.size() != 0) {
+			if (((Trade) listEntities.get(0)).getStatus() != TradeStatus.DENIED && ((Trade) listEntities.get(0)).getStatus() != TradeStatus.REPLACEMENT_DELIVERED) {
+				return "Esse produto já faz parte de uma troca em andamento";
+			}
+		}
+		
+		trade.setType(originalTradeType);
+		System.out.println("after" + ((Trade) entity).getType());
 		return null;
 	}
 
