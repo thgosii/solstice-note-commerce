62f3632f0a340735461c9622fd5f84c2335d05dc
Thiago Bezerra <thiago.bezsilva@gmail.com>
2020-06-02 10:27:43 -0300

    validate payment, check if the value is less than 10

--- src/br/com/solstice/notecommerce/controller/strategy/impl/domain/sale/ValidatePayment.java
+++ src/br/com/solstice/notecommerce/controller/strategy/impl/domain/sale/ValidatePayment.java
@@ -3,6 +3,7 @@ package br.com.solstice.notecommerce.controller.strategy.impl.domain.sale;
 import br.com.solstice.notecommerce.controller.strategy.IStrategy;
 import br.com.solstice.notecommerce.entity.Entity;
 import br.com.solstice.notecommerce.entity.domain.shop.sale.Sale;
+import br.com.solstice.notecommerce.entity.domain.shop.sale.SaleCreditCard;
 
 public class ValidatePayment implements IStrategy {
 
@@ -10,7 +11,13 @@ public class ValidatePayment implements IStrategy {
 	public String process(Entity entity) {
 		Sale sale = (Sale) entity;
 
-		if (sale.getBalanceUsage() < 0.0) {
+		if (sale.getCreditCards().get(0).getCreditCard().getId() != null) {
+			for (SaleCreditCard saleCreditCard : sale.getCreditCards()) {
+				if (saleCreditCard.getValue() < 10.0) {
+					return "Todos os cartões devem possuir pelo menos 10 reais";
+				}
+			}
+		} else if (sale.getBalanceUsage() < 0.0) {
 			return "Selecione um meio de pagamento válido";
 		} else if (sale.getBalanceUsage() >= sale.getTotal()) {
 			return "O valor em saldo digital deve ser menor que o total";
