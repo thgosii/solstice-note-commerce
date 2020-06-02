4e2469c192ef702bf9bb896046d2407ac0270c9b
g-otn <g-otn@hotmail.com>
2020-06-02 06:10:10 -0300

    feat: DAO methods for Stock and StockUpdate

--- src/br/com/solstice/notecommerce/dao/impl/domain/stock/StockDAO.java
+++ src/br/com/solstice/notecommerce/dao/impl/domain/stock/StockDAO.java
@@ -1,10 +1,18 @@
 package br.com.solstice.notecommerce.dao.impl.domain.stock;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.solstice.notecommerce.dao.AbstractDAO;
+import br.com.solstice.notecommerce.dao.impl.domain.product.ProductDAO;
 import br.com.solstice.notecommerce.entity.Entity;
+import br.com.solstice.notecommerce.entity.domain.product.Product;
+import br.com.solstice.notecommerce.entity.domain.stock.Stock;
 
 public class StockDAO extends AbstractDAO {
 	
@@ -16,22 +24,150 @@ public class StockDAO extends AbstractDAO {
 		super(connection);
 	}
 
+	// Called automatically on product save
 	@Override
 	public long save(Entity entity) {
-		// TODO Auto-generated method stub
+		openConnection();
+		PreparedStatement pstm = null;
+
+		Stock stock = (Stock) entity;
+
+		String sql = "INSERT INTO `notecommerce_db`.`stock` (`stk_prd_id`, `stk_quantity`) VALUES (?, ?)";
+		
+		try {
+			pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+			pstm.setLong(1, stock.getProduct().getId());
+			pstm.setInt(2, stock.getQuantity());
+
+			System.out.println("  " + this.getClass().getSimpleName() + "#" + new Exception().getStackTrace()[0].getMethodName() + ": " + pstm.toString());
+
+			pstm.execute();
+
+			ResultSet rs = pstm.getGeneratedKeys();
+
+			long idStock = 0;
+
+			if (rs.next()) {
+				idStock = rs.getLong(1);
+			}
+
+			return idStock;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (pstm != null) {
+				try {
+					pstm.close();
+				} catch (SQLException e) {
+				}
+			}
+			closeConnection();
+		}
+
 		return 0;
 	}
 
 	@Override
 	public void remove(Entity entity) {
 	}
-
+	
+	// Called by sale/trade strategies and stock up form
 	@Override
 	public void update(Entity entity) {
+		openConnection();
+		PreparedStatement pstm = null;
+
+		Stock stock = (Stock) entity;
+
+		String sql = "UPDATE `notecommerce_db`.`stock` SET `stk_quantity` = ?' WHERE (`stk_prd_id` = ?)";
+		
+		try {
+			pstm = connection.prepareStatement(sql);
+			
+			pstm.setInt(1, stock.getQuantity());
+			pstm.setLong(2, stock.getProduct().getId());
+
+			System.out.println("  " + this.getClass().getSimpleName() + "#" + new Exception().getStackTrace()[0].getMethodName() + ": " + pstm.toString());
+
+			pstm.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (pstm != null) {
+				try {
+					pstm.close();
+				} catch (SQLException e) {
+				}
+			}
+			closeConnection();
+		}
 	}
 
 	@Override
 	public List<Entity> consult(Entity entity, String operation) {
+		openConnection();
+
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+
+		Stock stock = (Stock) entity;
+
+		String sql = "";
+
+		if (operation.equals("consult")) { // Gets trade table data
+			if (stock.getProduct() != null && stock.getProduct().getId() != null) {
+				// Stock of a single product
+				sql = "SELECT * FROM notecommerce_db.stock WHERE stk_prd_id = ?";
+			} else {
+				// All products stock
+				sql = "SELECT * FROM notecommerce_db.stock";
+			}
+		}
+		
+		List<Entity> stocks = new ArrayList<Entity>();
+
+		try {
+			pstm = connection.prepareStatement(sql);
+
+			if (operation.equals("consult") && stock.getProduct() != null && stock.getProduct().getId() != null) {
+				pstm.setLong(1, stock.getProduct().getId());
+			}
+			
+			System.out.println("  " + this.getClass().getSimpleName() + "#" + new Exception().getStackTrace()[0].getMethodName() + ": " + pstm.toString().substring(pstm.toString().indexOf(':') + 2));
+
+			rs = pstm.executeQuery();
+			
+			ProductDAO productDAO = new ProductDAO(connection);
+
+			while (rs.next()) {
+				Stock currentStock = new Stock();
+				
+				if (operation.equals("consult")) {
+					stock.setId(rs.getLong("stk_id"));
+					stock.setQuantity(rs.getInt("stk_quantity"));
+					
+					Product product = new Product();
+					product.setId(rs.getLong("stk_prd_id"));
+					stock.setProduct((Product) productDAO.consult(product, "consult").get(0));
+				}
+				
+				stocks.add(currentStock);
+			}
+
+			return stocks;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (pstm != null) {
+				try {
+					pstm.close();
+				} catch (SQLException e) {
+				}
+			}
+			closeConnection();
+		}
+
 		return null;
 	}
 
--- src/br/com/solstice/notecommerce/dao/impl/domain/stock/StockUpdateDAO.java
+++ src/br/com/solstice/notecommerce/dao/impl/domain/stock/StockUpdateDAO.java
@@ -1,10 +1,19 @@
 package br.com.solstice.notecommerce.dao.impl.domain.stock;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.com.solstice.notecommerce.dao.AbstractDAO;
 import br.com.solstice.notecommerce.entity.Entity;
+import br.com.solstice.notecommerce.entity.domain.stock.Stock;
+import br.com.solstice.notecommerce.entity.domain.stock.StockUpdate;
 
 public class StockUpdateDAO extends AbstractDAO {
 	
@@ -18,7 +27,45 @@ public class StockUpdateDAO extends AbstractDAO {
 
 	@Override
 	public long save(Entity entity) {
-		// TODO Auto-generated method stub
+		openConnection();
+		PreparedStatement pstm = null;
+
+		StockUpdate stockUpdate = (StockUpdate) entity;
+
+		String sql = "INSERT INTO `notecommerce_db`.`stock_history` (`sth_stk_id`, `sth_quantity`, `sth_date`) VALUES (?, ?, ?)";
+		
+		try {
+			pstm = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+			pstm.setLong(1, stockUpdate.getStock().getId());
+			pstm.setInt(2, stockUpdate.getQuantity());
+			pstm.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
+
+			System.out.println("  " + this.getClass().getSimpleName() + "#" + new Exception().getStackTrace()[0].getMethodName() + ": " + pstm.toString());
+
+			pstm.execute();
+
+			ResultSet rs = pstm.getGeneratedKeys();
+
+			long idStockUpdate = 0;
+
+			if (rs.next()) {
+				idStockUpdate = rs.getLong(1);
+			}
+
+			return idStockUpdate;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (pstm != null) {
+				try {
+					pstm.close();
+				} catch (SQLException e) {
+				}
+			}
+			closeConnection();
+		}
+
 		return 0;
 	}
 
@@ -32,7 +79,60 @@ public class StockUpdateDAO extends AbstractDAO {
 
 	@Override
 	public List<Entity> consult(Entity entity, String operation) {
-		// TODO Auto-generated method stub
+		openConnection();
+
+		PreparedStatement pstm = null;
+		ResultSet rs = null;
+		
+		// not needed since stock history consult will always be the same query which doesn't need parameters
+		//StockUpdate stockUpdate = (StockUpdate) entity;
+
+		String sql = "";
+
+		if (operation.equals("consult")) {
+			sql = "SELECT * FROM notecommerce_db.stock_history";
+		}
+		
+		List<Entity> stocks = new ArrayList<Entity>();
+
+		try {
+			pstm = connection.prepareStatement(sql);
+
+			if (operation.equals("consult")) {
+			}
+			
+			System.out.println("  " + this.getClass().getSimpleName() + "#" + new Exception().getStackTrace()[0].getMethodName() + ": " + pstm.toString().substring(pstm.toString().indexOf(':') + 2));
+
+			rs = pstm.executeQuery();
+			
+			StockDAO stockDAO = new StockDAO(connection);
+
+			while (rs.next()) {
+				StockUpdate currentStockUpdate = new StockUpdate();
+				
+				if (operation.equals("consult")) {
+					currentStockUpdate.setId(rs.getLong("sth_id"));
+					currentStockUpdate.setStock((Stock) stockDAO.consult(new Stock(rs.getLong("sth_stk_id")), operation));
+					currentStockUpdate.setQuantity(rs.getInt("sth_quantity"));
+					currentStockUpdate.setDate(rs.getTimestamp("sth_date").toLocalDateTime());
+				}
+				
+				stocks.add(currentStockUpdate);
+			}
+
+			return stocks;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (pstm != null) {
+				try {
+					pstm.close();
+				} catch (SQLException e) {
+				}
+			}
+			closeConnection();
+		}
+
 		return null;
 	}
 
