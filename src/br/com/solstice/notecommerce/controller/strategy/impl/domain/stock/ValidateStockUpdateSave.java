d84a75e0c90d3c94e3d3f52bb8a912bbc0ad485d
g-otn <g-otn@hotmail.com>
2020-06-02 07:21:49 -0300

    fix: not validating stock update date

--- src/br/com/solstice/notecommerce/controller/strategy/impl/domain/stock/ValidateStockUpdateSave.java
+++ src/br/com/solstice/notecommerce/controller/strategy/impl/domain/stock/ValidateStockUpdateSave.java
@@ -1,5 +1,7 @@
 package br.com.solstice.notecommerce.controller.strategy.impl.domain.stock;
 
+import java.time.LocalDateTime;
+
 import br.com.solstice.notecommerce.controller.strategy.IStrategy;
 import br.com.solstice.notecommerce.entity.Entity;
 import br.com.solstice.notecommerce.entity.domain.stock.StockUpdate;
@@ -18,6 +20,11 @@ public class ValidateStockUpdateSave implements IStrategy {
 			sb.append("É necessário especificar a quantidade de alta/baixa do estoque\n");
 		} else if (stockUpdate.getQuantity() < 0) {
 			sb.append("A quantidade do produto no estoque é inválida\n");
+		}
+		if (stockUpdate.getDate() == null) {
+			sb.append("É necessário especificar a data e hora da alta/baixa no estoque");
+		} else if (stockUpdate.getDate().isAfter(LocalDateTime.now())) {
+			sb.append("Não é possível especificar uma data e hora de alta/baixa no estoque no futuro");
 		}
 		
 		return sb.toString().trim().isEmpty() ? null : sb.toString().trim();
