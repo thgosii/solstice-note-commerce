ec2d89835c11ed4fc14f8378c0e0f33daa9a8230
g-otn <g-otn@hotmail.com>
2020-06-15 04:10:20 -0300

    add: TODO

--- src/br/com/solstice/notecommerce/controller/viewhelper/impl/domain/stock/StockAdminVH.java
+++ src/br/com/solstice/notecommerce/controller/viewhelper/impl/domain/stock/StockAdminVH.java
@@ -10,6 +10,7 @@ import br.com.solstice.notecommerce.controller.viewhelper.IViewHelper;
 import br.com.solstice.notecommerce.entity.Entity;
 import br.com.solstice.notecommerce.entity.Result;
 
+// TODO(?): This was just in case we had time to add an extra admin page (or modify the product listing) to show the stock of each product, not in requirements document 
 public class StockAdminVH implements IViewHelper {
 
 	@Override
@@ -22,7 +23,6 @@ public class StockAdminVH implements IViewHelper {
 	public void setView(Result result, HttpServletRequest request, HttpServletResponse response)
 			throws IOException, ServletException {
 		// TODO Auto-generated method stub
-
 	}
 
 }
